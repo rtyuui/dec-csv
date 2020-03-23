764e0d9bc727ffa0f9eb1ff450045f6be92c326d
rtyuui <48699152+rtyuui@users.noreply.github.com>
2020-03-22 23:17:36 -0400

    code/poc

---
+++ csv.py
@@ -0,0 +1,35 @@
+
+def valid_file(given_file):
+    """
+
+    :param given_file: name of the supplied file
+    :return: if the file is of valid format
+    Note: Lazy Check
+    """
+    if ".csv" not in given_file:
+        print("Not a .csv type file? ")
+        return False
+    else:
+        return True
+
+
+filename = input("Filename: ")
+new_filename = filename[0: len(filename) - 4]
+#  dec_filename = new_filename + "_decrypted.csv"
+new_filename += "_decrypted.lzma"
+
+f = open(filename, "rb")
+data_enc = f.read(43)
+data = f.read()
+
+empty = b'\x00'
+
+new_data = data[25:]
+first_8 = new_data[:8]
+rest = new_data[12:]
+print(new_data)
+new_file = open(new_filename, "wb")
+new_file.write(first_8 + empty * 6 + b'\x11' + b'\x13' + rest)
+new_file.close()
+f.close()
+
