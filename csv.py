cea95060b70bd2f9b3fbda7392d0aa3f466f50ff
rtyuui <48699152+rtyuui@users.noreply.github.com>
2020-03-22 23:23:07 -0400

    coments

--- csv.py
+++ csv.py
@@ -19,17 +19,22 @@ new_filename = filename[0: len(filename) - 4]
 new_filename += "_decrypted.lzma"
 
 f = open(filename, "rb")
+# ignore first 43 bytes
 data_enc = f.read(43)
+# save rest of bytes
 data = f.read()
 
 empty = b'\x00'
+
 
 new_data = data[25:]
 first_8 = new_data[:8]
 rest = new_data[12:]
 print(new_data)
 new_file = open(new_filename, "wb")
-new_file.write(first_8 + empty * 6 + b'\x11' + b'\x13' + rest)
+# insert 6 bytes in between 
+new_file.write(first_8 + empty * 6 + b'\x11' + b'\x13' + rest) 
+# save & finish
 new_file.close()
 f.close()
 
